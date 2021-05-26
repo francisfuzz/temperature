44dd3d8e9204c6df0f4bdc14b3b3477211f641ae
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-05-26 18:37:29 +0000

    lint: addPropTypes for BoilingVerdict

--- src/components/BoilingVerdict.js
+++ src/components/BoilingVerdict.js
@@ -1,16 +1,17 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 class BoilingVerdict extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    if (props.celsius >= 100) {
+  render () {
+    if (this.props.celsius >= 100) {
       return <p>The water would boil.</p>
     }
     return <p>The water would not boil.</p>
   }
+}
+
+BoilingVerdict.propTypes = {
+  celsius: PropTypes.number.isRequired
 }
 
 export default BoilingVerdict
