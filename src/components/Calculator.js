060464de053a7fcdb5298cb0a90d2443e621b5a4
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-05-26 19:15:02 +0000

    refactor: move temp conversions in own utility file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,27 +1,7 @@
 import React from 'react'
 import BoilingVerdict from './BoilingVerdict'
 import TemperatureInput from './TemperatureInput'
-
-function toCelsius (fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9
-}
-
-function toFahrenheit (celsius) {
-  return (celsius * 9 / 5) + 32
-}
-
-function tryConvert (temperature, convert) {
-  const input = parseFloat(temperature)
-
-  // Only convert number inputs. Return an empty string otherwise.
-  if (Number.isNaN(input)) {
-    return ''
-  }
-
-  const output = convert(input)
-  const rounded = Math.round(output * 1000) / 1000
-  return rounded.toString()
-}
+import { toCelsius, toFahrenheit, tryConvert } from '../utils/temperature-conversions'
 
 class Calculator extends React.Component {
   constructor (props) {
---
+++ src/utils/temperature-conversions.js
@@ -0,0 +1,22 @@
+function toCelsius (fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9
+}
+
+function toFahrenheit (celsius) {
+  return (celsius * 9 / 5) + 32
+}
+
+function tryConvert (temperature, convert) {
+  const input = parseFloat(temperature)
+
+  // Only convert number inputs. Return an empty string otherwise.
+  if (Number.isNaN(input)) {
+    return ''
+  }
+
+  const output = convert(input)
+  const rounded = Math.round(output * 1000) / 1000
+  return rounded.toString()
+}
+
+export { toCelsius, toFahrenheit, tryConvert }
